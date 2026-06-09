846d9e5a7395f17c54a877eae3f975d57729e587
Tono <t.ono10200204@gmail.com>
2026-06-09 12:00:07 +0900

    part1 courseinfo: exercises 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,9 +5,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part = {props.part1}/>
-      <Part part = {props.part2}/>
-      <Part part = {props.part3}/>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -22,27 +22,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const total = part1.exercises + part2.exercises + part3.exercises
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1}
-        part2={part2}
-        part3={part3}/>
-      <Total total={total}/>
+      <Content parts={parts} />
+      <Total total={total} />
     </div>
   )
 }
